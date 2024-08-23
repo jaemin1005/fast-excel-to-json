6967186a6db7797e686d5649679f00791883142f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-24 00:22:50 +0900

    New | 유닉스 시간을 ISO로 변경하여 준다

---
+++ src/excel_time_func/unix_to_iso.rs
@@ -0,0 +1,25 @@
+use chrono::{TimeZone, Utc};
+
+pub fn unix_to_iso(unix_time: i64) -> String {
+    let datetime = Utc.timestamp_opt(unix_time, 0);
+
+    let time = datetime.single();
+    match time {
+        Some(result) => result.to_rfc3339(),
+        None => String::from("Invalid")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::excel_time_func::unix_to_iso::unix_to_iso;
+
+    #[test]
+    fn valid_unix_to_iso() {
+        let unix_time: i64= 1721692800;
+        let expect_string = "2024-07-23T00:00:00+00:00"; 
+
+        //* Unix Time GMT 2024.07.23 00:00:00 */
+        assert_eq!(unix_to_iso(unix_time), expect_string);
+    }
+}
