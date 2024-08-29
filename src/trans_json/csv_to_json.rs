d8c37777bb8c79108ddc159d602ca13b896cf6c6
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-29 21:35:34 +0900

    Style | Add Comment

--- src/trans_json/csv_to_json.rs
+++ src/trans_json/csv_to_json.rs
@@ -13,10 +13,11 @@ pub fn csv_to_json(csv_data: &[u8]) -> Vec<Map<String, Value>> {
     }
     .clone();
 
-    println!("{:?}", headers);
+    //println!("{:?}", headers);
 
     let mut records = Vec::new();
 
+    //* records는 header를 제외한 body 행들의 모음 */
     for record in reader.records() {
         match record {
             Ok(data) => records.push(data),
@@ -24,25 +25,31 @@ pub fn csv_to_json(csv_data: &[u8]) -> Vec<Map<String, Value>> {
         }
     }
 
-    println!("{:?}", records);
+    //println!("{:?}", records);
 
+    //* records와 헤더를 쌍으로 Map형태로 만든다 */
     let rows_json = records
         .into_par_iter()
-        .filter_map(|record| {
+        .map(|record| {
             let mut row_json = serde_json::Map::new();
 
             for (header, row) in headers.iter().zip(record.iter()) {
                 row_json.insert(header.to_string(), transform_string(row));
             }
 
-            Some(row_json)
+            row_json
         })
         .collect();
 
     rows_json
 }
 
+/**
+ * * transform_string
+ * * 문자열을 Rust의 각 타입으로 변환하여 Value로 반환한다.
+ */
 fn transform_string(value: &str) -> Value {
+    //* boolean 타입 검사 */
     let trans = match value.to_lowercase().as_str() {
         "true" => return Value::Bool(true),
         "false" => return Value::Bool(false),
@@ -51,6 +58,7 @@ fn transform_string(value: &str) -> Value {
 
     let dot_count = trans.matches('.').count();
 
+    //* 점 개수를 판단하여, float, int로 변환한다. 변환이 실패할 경우 문자열을 반환 */
     match dot_count {
         0 => match trans.parse::<i64>() {
             Ok(num) => return Value::Number(Number::from(num)),
